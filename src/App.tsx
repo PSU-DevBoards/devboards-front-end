370fa023a447594a60c10a3faa09729174c876dd
kpjVideo <masonticehurst@me.com>
2021-09-22 23:34:13 -0400

    AB#4: Adds landing page to new /pages directory, illustration asset, and app.tsx routing (#5)
    * Added react-router for multipage navigation, added eslint-config-react-app to fix airbnb linting error
    
    * Adds Auth0 SDK for React
    
    * Revert "Adds Auth0 SDK for React"
    
    This reverts commit 0bbe4423fe5b94f535541b40e1b6c7d2a1b6304c.
    
    * Avoid merge conflict (remove eslint-config-react-app dependency)
    
    * Added landing page, router route, and landing illustration asset
    
    * conflict avoidance

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Landing from './pages/Landing';
 
 function App() {
-  return <div className="App">Hello World!</div>;
+  return (
+      <div className="App">
+        <Router>
+          <Route path="/" component={Landing} exact/>
+        </Router>
+      </div>
+  );
 }
 
 export default App;
