bc309dbd144a7c0d8736edd3fb3159defa308547
KappaJ <masonticehurst@me.com>
2021-10-03 13:33:05 -0400

    removed auth page as it is not needed here

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Landing from './pages/Landing';
-import Auth from './pages/Auth';
 
 function App() {
   return (
       <div className="App">
         <Router>
           <Route path="/" component={Landing} exact/>
-          <Route path="/auth" component={Auth} exact/>
         </Router>
       </div>
   );
